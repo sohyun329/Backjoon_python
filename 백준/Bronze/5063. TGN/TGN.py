e05e85384c8985b5a3912c86d7cf46b761abcef2
sohyun329 <58158103+sohyun329@users.noreply.github.com>
2024-01-10 16:33:44 +0900

    [Bronze III] Title: TGN, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,6 @@
+n = int(input())
+for i in range(n):
+    r,e,c = map(int,input().split())
+    if r+c < e : print('advertise')
+    elif r+c == e : print('does not matter')
+    else : print('do not advertise')
