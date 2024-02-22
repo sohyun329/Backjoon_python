ef6c2723a0a29dd76fd696287f619c480bb23feb
sohyun329 <58158103+sohyun329@users.noreply.github.com>
2024-02-22 16:14:52 +0900

    [Gold IV] Title: 플로이드, Time: 4548 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,23 @@
+INF = int(1e9)
+
+N = int(input())
+M = int(input())
+
+graph = [[INF] * (N+1) for _ in range(N+1)]
+for i in range(1, N+1):
+    graph[i][i] = 0
+    
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, N+1):
+    for a in range(1, N+1):
+        for b in range(1, N+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+                
+for x in range(1, N+1):
+    for y in range(1, N+1):
+        if graph[x][y] == INF: print(0, end=' ')
+        else: print(graph[x][y], end=' ')
+    print()
