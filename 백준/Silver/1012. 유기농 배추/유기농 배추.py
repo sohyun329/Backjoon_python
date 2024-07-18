6253ae360c078383e052a6b05ff03cb4e8d5137b
sohyun329 <58158103+sohyun329@users.noreply.github.com>
2024-07-18 14:43:17 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 31644 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,11 +1,11 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10000)  # 재귀 깊이 설정
 
 # test case
 T = int(sys.stdin.readline().strip())
 
-def make_farm(M,N,K):
-    farm = [[0]*M for _ in range(N)]
+def make_farm(M, N, K):
+    farm = [[0] * M for _ in range(N)]
     
     for _ in range(K):
         X, Y = map(int, sys.stdin.readline().split())
@@ -13,35 +13,27 @@ def make_farm(M,N,K):
     
     return farm
 
-def bfs(farm, start_x, start_y, M, N):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+def dfs(farm, x, y, M, N):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    queue = deque([(start_x, start_y)])
-    farm[start_y][start_x] = 0
+    # 현재 위치 방문 처리
+    farm[y][x] = 0
     
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                continue
-            
-            if farm[ny][nx] == 1:
-                farm[ny][nx] = 0
-                queue.append((nx,ny))
-                
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if 0 <= nx < M and 0 <= ny < N and farm[ny][nx] == 1:
+            dfs(farm, nx, ny, M, N)
 
 for _ in range(T):
     M, N, K = map(int, sys.stdin.readline().split())
-    farm = make_farm(M,N,K)
+    farm = make_farm(M, N, K)
     result = 0
     for start_y in range(N):
         for start_x in range(M):
             if farm[start_y][start_x] == 1:
-                bfs(farm, start_x, start_y, M, N)
+                dfs(farm, start_x, start_y, M, N)
                 result += 1
-                
     print(result)
