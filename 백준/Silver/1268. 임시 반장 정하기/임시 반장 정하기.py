705a599f39fd4046d3a9fbdcc3c75021c0200747
sohyun329 <58158103+sohyun329@users.noreply.github.com>
2024-10-07 11:41:12 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 408 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -2,18 +2,15 @@ import sys
 
 n = int(input())
 info = list(list(map(int, sys.stdin.readline().split())) for _ in range(n))
-ans = [[0 for _ in range(n)] for _ in range(n)]
+ans = [0]*n
 
-for k in range(5):
-    for i in range(n):
-        for j in range(i+1, n):
-            if info[i][k] == info[j][k]:
-                ans[i][j] = 1
-                ans[j][i] = 1
-                
+for i in range(n):
+    for j in range(i+1, n):
+        for g in range(5):
+            if info[i][g] == info[j][g]:
+                ans[i]+=1
+                ans[j]+=1 
+                break              
 
-cnt = []
-for a in ans:
-    cnt.append(a.count(1))
     
-print(cnt.index(max(cnt))+1)
+print(ans.index(max(ans))+1)
